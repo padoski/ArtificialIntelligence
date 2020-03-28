c0ad31ff041d0ca77bbd3a5e92783ce6be4ac4c5
Pawel <pawel.gladysz1999@onet.pl>
2020-03-28 17:18:08 +0100

    Bayes Theorem Implementation Using Data of wheater

---
+++ Bayes/Bayes/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bayes
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        { 
+            Bayes b = new Bayes();
+            b.ReadFromFiles();
+           
+            b.Propability("deszczowo", "chłodno", "mocny");
+            Console.ReadKey();
+
+        }
+
+    }
+    class Bayes
+    {
+        private string[] _lines = File.ReadAllLines(@"E:\SemestrIV\AI\Zajecia\Bayes\dane.txt");
+        private string[][] _data = new string[19][];
+        private double[] _counters=new double[5] {0,0,0,0,0};
+
+        public void ReadFromFiles()
+        {
+            for (int i = 0; i < _lines.Length; i++)
+            {
+                string[] tmp = _lines[i].Split(';');
+                _data[i] = new string[tmp.Length];
+                for (int j = 0; j < tmp.Length; j++)
+                {
+                    _data[i][j] = tmp[j];
+                    //Console.WriteLine(_data[i][j]);
+                }
+            }
+        }
+        public void Propability(string a1, string a2, string a3)
+        {
+
+            for (int i = 0; i < _lines.Length; i++)
+            {
+                if (_data[i][0] == a1) { _counters[0]++; }
+                if (_data[i][1] == a2) { _counters[1]++; }
+                if (_data[i][2] == a3) { _counters[2]++; }
+                if (_data[i][3] == "tak") { _counters[3]++; }
+                if (_data[i][3] == "nie") { _counters[4]++; }
+            }
+
+            double[] pC1a = new double[3];
+            double[] pC2a = new double[3];
+           /* for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(_counters[i]);
+            }*/
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (_counters[i] == 0)
+                {
+                    pC1a[i] = 1 / (_counters[3] + 3);
+                }
+                else
+                {
+                    pC1a[i] = _counters[i] / _counters[3];
+                }
+                if (_counters[i] == 0)
+                {
+                    pC2a[i] = 1 / (_counters[4] + 3);
+                }
+                else
+                {
+                    pC2a[i] = _counters[i] / _counters[4];
+                }
+                
+            }
+           /* for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(pC1a[i]);
+                Console.WriteLine(pC2a[i]);
+            }
+            */
+
+            double pC1 = _counters[3] / _lines.Length;
+            double pC2 = _counters[4] / _lines.Length;
+            double propC1;
+            double propC2;
+
+            propC1 =  pC1a[0]*pC1a[1]* pC1a[2]*pC1;
+            propC2 = pC2a[0] * pC2a[1] * pC2a[2] * pC2;
+
+            Console.WriteLine(propC1);
+            Console.WriteLine(propC2);
+            if (propC1>propC2)
+            {
+                Console.WriteLine("Decyzja to TAK");
+            }
+            else
+            {
+                Console.WriteLine("Decyzja to NIE");
+            }
+            
+        }
+    }
+}
+
