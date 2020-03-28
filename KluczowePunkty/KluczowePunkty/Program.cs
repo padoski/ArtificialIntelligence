f55e270b37ebc39fc4f13caf641aa09b5da2c00d
Pawel <pawel.gladysz1999@onet.pl>
2020-03-29 00:42:17 +0100

    Addes changes in structure of PotencialPoints

--- KluczowePunkty/KluczowePunkty/Program.cs
+++ KluczowePunkty/KluczowePunkty/Program.cs
@@ -19,72 +19,70 @@ namespace KluczowePunkty
     {
         public void AddMask()
         {
-            Bitmap btm = new Bitmap(@"E:\SemestrIV\AI\Zajecia\KluczowePunkty\KluczowePunkty\photo.png");
-            double[][] filterMatrix = new double[3][];
-            filterMatrix[0] = new double[] { -1, -1, -1 };
-            filterMatrix[1] = new double[] { -1, 8, -1 };
-            filterMatrix[2] = new double[] { -1, -1, -1 };
+            Bitmap btm = new Bitmap(@"E:\SemestrIV\AI\Zajecia\KluczowePunkty\KluczowePunkty\przejscie.png");
+            Bitmap newimage = new Bitmap(btm.Width,btm.Height);
+            double[][] filtr = new double[3][];
+            filtr[0] = new double[] { -1, -1, -1 };
+            filtr[1] = new double[] { -1, 8, -1 };
+            filtr[2] = new double[] { -1, -1, -1 };
             for (int i = 1; i < btm.Width-1; i++)
             {
-                Console.WriteLine("asd");
                 for (int j = 1; j < btm.Height-1; j++)
                 {
-                    /////
-
-
-                    int newR = 0;
-                    int newG = 0;
-                    int newB = 0;
+                    int RValue = 0;
+                    int GValue = 0;
+                    int BValue = 0;
                     for (int k = -1; k < 2; k++)
                     {
                         for (int l = -1; l < 2; l++)
                         {
-                            int mask = (int)filterMatrix[k+1][l+1];
-                            newR += btm.GetPixel(i + k, j + l).R * mask;
-                            newG += btm.GetPixel(i + k, j + l).G * mask;
-                            newB += btm.GetPixel(i + k, j + l).B * mask;
+                            int mask = (int)filtr[k+1][l+1];
+                            RValue += btm.GetPixel(i + k, j + l).R * mask;
+                            GValue += btm.GetPixel(i + k, j + l).G * mask;
+                            BValue += btm.GetPixel(i + k, j + l).B * mask;
                         }
                     }
-                    newR = CompareRGB(newR);
-                    newG = CompareRGB(newG);
-                    newB = CompareRGB(newB);
-                    btm.SetPixel(i, j, Color.FromArgb(newR, newG, newB));
+                    if (RValue > 255)
+                    { RValue = 255; }
+                    else if (RValue < 0)
+                    { RValue = 0; }
+
+                    if (GValue > 255)
+                    { GValue = 255; }
+                    else if (GValue < 0)
+                    { GValue = 0; }
+
+                    if (BValue > 255)
+                    { BValue = 255; }
+                    else if (BValue < 0)
+                    { BValue = 0; }
+
+                    newimage.SetPixel(i, j, Color.FromArgb(RValue, GValue, BValue));
                 }
             }
-            btm.Save(@"E:\SemestrIV\AI\Zajecia\KluczowePunkty\KluczowePunkty\NewPhoto1.png");
-        }
-        public int CompareRGB(int val)
+            newimage.Save(@"E:\SemestrIV\AI\Zajecia\KluczowePunkty\KluczowePunkty\NewPhoto2.png");
+        } 
+        public void PotencialPoints(int numberOfPoints)
         {
-            if (val > 255)
-                return 255;
-            if (val < 0)
-                return 0;
-            return val;
-        }
-        public void PotencialPoints(int pointCount)
-        {
-            Bitmap btm = new Bitmap(@"E:\SemestrIV\AI\Zajecia\KluczowePunkty\KluczowePunkty\NewPhoto1.png");
+            Bitmap btm = new Bitmap(@"E:\SemestrIV\AI\Zajecia\KluczowePunkty\KluczowePunkty\NewPhoto2.png");
             Bitmap result = new Bitmap(btm.Width, btm.Height);
-            List<Point> keyPointCandidates = new List<Point>();
-
+            List<List<double>> potencialPoints = new List<List<double>>();
 
             for (int i = 1; i < btm.Width - 1; i++)
             {
                 for (int k = 1; k < btm.Height - 1; k++)
                 {
-                    keyPointCandidates.Add(
-                        new Point
+                    potencialPoints.Add(
+                        new List<double>
                         {
-                            x = i,
-                            y = k,
-                            brightness = CalculateBrightness(btm, i, k)
+                            Convert.ToDouble(i), Convert.ToDouble(k),Convert.ToDouble(BrightnessCalculate(btm, i, k))
                         }
                         ); ;
                 }
             }
 
-            keyPointCandidates = keyPointCandidates.OrderByDescending(k => k.brightness).ToList();
-            keyPointCandidates.RemoveRange(pointCount, keyPointCandidates.Count - pointCount);
+            potencialPoints = potencialPoints.OrderByDescending(k => k[2]).ToList();
+            potencialPoints.RemoveRange(numberOfPoints, potencialPoints.Count - numberOfPoints);
 
             for (int i = 0; i < result.Width; i++)
             {
@@ -94,12 +92,12 @@ namespace KluczowePunkty
                 }
             }
 
-            foreach (var p in keyPointCandidates)
+            foreach (var p in potencialPoints)
             {
-                result.SetPixel(p.x, p.y, Color.FromArgb(255, 0, 0));
+                result.SetPixel(Convert.ToInt32(p[0]), Convert.ToInt32(p[1]), Color.FromArgb(255, 0, 0));
             }
 
-            result.Save(@"E:\SemestrIV\AI\Zajecia\KluczowePunkty\KluczowePunkty\photoKeyPoints1.png");
+            result.Save(@"E:\SemestrIV\AI\Zajecia\KluczowePunkty\KluczowePunkty\photoKeyPoints2.png");
 
         }
         public void CopyPixel(ref Bitmap dest, int x, int y, Bitmap source, int a, int b)
@@ -107,7 +105,7 @@ namespace KluczowePunkty
             Color pxl = source.GetPixel(a, b);
             dest.SetPixel(x, y, pxl);
         }
-        public float CalculateBrightness(Bitmap img, int x, int y)
+        public float BrightnessCalculate(Bitmap img, int x, int y)
         {
             float result = 0;
 
@@ -118,15 +116,7 @@ namespace KluczowePunkty
                     result += img.GetPixel(x + a, y + b).GetBrightness();
                 }
             }
-
-            //We are calculating pixels in 3x3 matrix around pixel(x,y)
             return result / 9;
-        }
-        public struct Point
-        {
-            public int x;
-            public int y;
-            public float brightness;
         }
     }
     
