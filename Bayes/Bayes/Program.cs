e9fa5d9d821ba9d852740d2f1b6c1f06ff842411
Pawel <pawel.gladysz1999@onet.pl>
2020-03-30 14:13:58 +0200

    Fix bugs

--- Bayes/Bayes/Program.cs
+++ Bayes/Bayes/Program.cs
@@ -15,20 +15,23 @@ namespace Bayes
             Bayes b = new Bayes();
             b.ReadFromFiles();
            
-            b.Propability("deszczowo", "chłodno", "mocny");
+            b.Propability("pochmurno", "chłodno", "mocny");
             Console.ReadKey();
-
         }
 
     }
     class Bayes
     {
+        int temp1 { get; set; }
         private string[] _lines = File.ReadAllLines(@"E:\SemestrIV\AI\Zajecia\Bayes\dane.txt");
-        private string[][] _data = new string[19][];
+        private string[][] _data;
         private double[] _counters=new double[5] {0,0,0,0,0};
+        private double[] _counterOfSame = new double[3] { 0, 0, 0 };
+
 
         public void ReadFromFiles()
         {
+            _data = new string[_lines.Length][];
             for (int i = 0; i < _lines.Length; i++)
             {
                 string[] tmp = _lines[i].Split(';');
@@ -48,43 +51,50 @@ namespace Bayes
                 if (_data[i][0] == a1) { _counters[0]++; }
                 if (_data[i][1] == a2) { _counters[1]++; }
                 if (_data[i][2] == a3) { _counters[2]++; }
+                if (_data[i][0] == a1 && _data[i][3] == "tak") { _counterOfSame[0]++; }
+                if (_data[i][1] == a2 && _data[i][3] == "tak") { _counterOfSame[1]++; }
+                if (_data[i][2] == a3 && _data[i][3] == "tak") { _counterOfSame[2]++; }
                 if (_data[i][3] == "tak") { _counters[3]++; }
                 if (_data[i][3] == "nie") { _counters[4]++; }
             }
 
             double[] pC1a = new double[3];
             double[] pC2a = new double[3];
-           /* for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine(_counters[i]);
-            }*/
-
+                Console.WriteLine("Liczniki" +i+" "+_counters[i]);
+            }
             for (int i = 0; i < 3; i++)
             {
-                if (_counters[i] == 0)
+                if (_counterOfSame[i]==0)
                 {
                     pC1a[i] = 1 / (_counters[3] + 3);
                 }
                 else
                 {
-                    pC1a[i] = _counters[i] / _counters[3];
-                }
-                if (_counters[i] == 0)
+                    pC1a[i] = _counterOfSame[i] / _counters[3] ;
+                }             
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                if (_counters[i] -_counterOfSame[i] == 0)
                 {
                     pC2a[i] = 1 / (_counters[4] + 3);
                 }
                 else
                 {
-                    pC2a[i] = _counters[i] / _counters[4];
+                    pC2a[i] = (_counters[i] - _counterOfSame[i]) / _counters[4] ;
                 }
-                
             }
-           /* for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 3; i++)
             {
-                Console.WriteLine(pC1a[i]);
-                Console.WriteLine(pC2a[i]);
+                Console.WriteLine("PC1Ai" + i + " "+pC1a[i]);
             }
-            */
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("PC1A2" + i + " " + pC2a[i]);
+            }
+
 
             double pC1 = _counters[3] / _lines.Length;
             double pC2 = _counters[4] / _lines.Length;
@@ -100,10 +110,11 @@ namespace Bayes
             {
                 Console.WriteLine("Decyzja to TAK");
             }
-            else
+            else 
             {
                 Console.WriteLine("Decyzja to NIE");
             }
+           
             
         }
     }
